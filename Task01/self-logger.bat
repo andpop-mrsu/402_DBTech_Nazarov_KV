9b8c0a684f2fbb479f90fb13464d4d942353d56d
Hazzardgg <kirya_nazarov_2002@mail.ru>
2023-09-16 21:27:06 +0300

    v1.0

--- Task01/self-logger.bat
+++ Task01/self-logger.bat
@@ -1,37 +1,26 @@
-#!/bin/bash
+@echo off
+chcp 65001 > nul
 
-# Путь к файлу базы данных SQLite
-database_path="self_logger.db"
+set DB_PATH=self_logger.db
 
-# Проверяем, существует ли файл базы данных, и создаем его, если он не существует
-if [ ! -e "$database_path" ]; then
-    sqlite3 "$database_path" <<EOF
-CREATE TABLE logs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user TEXT,
-    date DATETIME
-);
-EOF
-fi
+if not exist %DB_PATH% (
+    sqlite3.exe %DB_PATH% "CREATE TABLE IF NOT EXISTS logs (user TEXT, date TEXT);"
+)
 
-# Получаем имя текущего пользователя
-current_user=$(whoami)
+set "user=%USERNAME%"
 
-# Записываем информацию о запуске в базу данных
-sqlite3 "$database_path" <<EOF
-INSERT INTO logs (user, date) VALUES ('$current_user', datetime('now'));
-EOF
+sqlite3.exe %DB_PATH% "INSERT INTO logs (user, date) VALUES ('%user%', datetime('now', 'localtime'));"
 
-# Выводим информацию на экран
-program_name=$(basename "$0")
-total_runs=$(sqlite3 "$database_path" "SELECT COUNT(*) FROM logs;")
-first_run=$(sqlite3 "$database_path" "SELECT MIN(date) FROM logs;")
-echo "Имя программы: $program_name"
-echo "Количество запусков: $total_runs"
-echo "Первый запуск: $first_run"
-echo "---------------------------------------------"
-echo "User      | Date"
-echo "--------------------------------------------"
-sqlite3 "$database_path" "SELECT user, date FROM logs;" | while IFS="|" read -r user date; do
-    printf "%-10s | %s\n" "$user" "$date"
-done
+echo Имя программы: %~nx0
+
+echo|<nul set /p="Количество запусков: "
+sqlite3.exe %DB_PATH% "SELECT COUNT(*) AS 'Количество запусков' FROM logs;"
+
+echo|<nul set /p=" Первый запуск: "
+sqlite3.exe %DB_PATH% "SELECT MIN(date) AS 'Дата первого запуска' FROM logs;"
+
+echo ---------------------------------------------
+echo User      ^| Date
+echo ---------------------------------------------
+
+sqlite3.exe %DB_PATH% "SELECT DISTINCT user, date FROM logs;"
